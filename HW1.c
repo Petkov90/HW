b187beb6c7dafd91e4803f581d45012236c8119a
Petkov90 <118366461+Petkov90@users.noreply.github.com>
2022-11-17 17:12:26 +0200

    Add files via upload

---
+++ HW1.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+int main() {
+ 
+        printf(" Petko\n PYP\n Head of mintenance\n");
+return 0;
+}
---
+++ HW2.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main() {
+	int side1, side2, lice, obikolka;
+    side1 = 3; 
+    side2 = 5; 
+    lice = side1 * side2;
+    obikolka = side1 * 2 + side2 * 2;
+        printf(" lice:%d\n obikolka:%d\n ", lice , obikolka);
+return 0;
+}
